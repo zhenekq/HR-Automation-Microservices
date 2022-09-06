de5ce9aacc744fdb5bddc46039b533f882c3dffd
yauheni.vozny <zhenek02ss@gmail.com>
2022-09-06 20:47:25 +0300

    feat: try to see, does kubernetes works!

---
+++ user-service/src/main/java/com/mifort/hr/automation/microservices/userservice/request/BaseController.java
@@ -0,0 +1,12 @@
+package com.mifort.hr.automation.microservices.userservice.request;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+    @GetMapping
+    public String kubernetesWorks(){
+        return "WORKS REALLY IT IS NEW CONTROLLER!";
+    }
+}
