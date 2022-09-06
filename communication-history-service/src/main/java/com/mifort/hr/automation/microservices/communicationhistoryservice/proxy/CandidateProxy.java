4aee2e1df4e63f6c250b842ef4008bedc32dad8e
yauheni.vozny <zhenek02ss@gmail.com>
2022-09-06 15:38:41 +0300

    feat: add postgresql heroku db

--- communication-history-service/src/main/java/com/mifort/hr/automation/microservices/communicationhistoryservice/proxy/CandidateProxy.java
+++ communication-history-service/src/main/java/com/mifort/hr/automation/microservices/communicationhistoryservice/proxy/CandidateProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "candidate-service", url = "${CANDIDATE_SERVICE_SERVICE_HOST:http://localhost}:8000/api/v1/candidate")
+@FeignClient(name = "candidate-service", url = "${CANDIDATE_SERVICE_HOST:http://localhost}:8000/api/v1/candidate")
 public interface CandidateProxy {
     @GetMapping("/{candidateId}")
     CandidateResponse getById(@PathVariable String candidateId);
