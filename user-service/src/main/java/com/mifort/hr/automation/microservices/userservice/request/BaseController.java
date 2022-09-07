7e524f43be4a6a032174669c27e66feb220b13f3
yauheni.vozny <zhenek02ss@gmail.com>
2022-09-07 19:02:06 +0300

    commit

--- user-service/src/main/java/com/mifort/hr/automation/microservices/userservice/request/BaseController.java
+++ user-service/src/main/java/com/mifort/hr/automation/microservices/userservice/request/BaseController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class BaseController {
     @GetMapping
     public String kubernetesWorks(){
-        return "CI / CD IS WORKING";
+        return "Hello, Andrew. CI / CD Is works!!!";
     }
 }
