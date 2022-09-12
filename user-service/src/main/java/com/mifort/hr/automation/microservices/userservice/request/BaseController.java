41c9607bddc5cb08591d5c3488d0b430329809be
yauheni.vozny <zhenek02ss@gmail.com>
2022-09-12 15:07:57 +0300

    last chance v2

--- user-service/src/main/java/com/mifort/hr/automation/microservices/userservice/request/BaseController.java
+++ user-service/src/main/java/com/mifort/hr/automation/microservices/userservice/request/BaseController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class BaseController {
     @GetMapping
     public String kubernetesWorks(){
-        return "LAST CHANCE";
+        return "LAST CHANCE v2";
     }
 }
